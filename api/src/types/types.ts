16c956610c27827b5fbddaca80a263fc4f75bc31
theMillenniumFalcon <nishankpr2002@gmail.com>
2022-06-11 19:57:41 +0530

    auth api done

--- api/src/types/types.ts
+++ api/src/types/types.ts
@@ -1,7 +1,9 @@
 import { Request, Response } from 'express'
 import { Session, SessionData } from "express-session";
+import { Redis } from "ioredis";
 
 export type Context = {
     req: Request & { session: Session & Partial<SessionData> & { userId: number } }
+    redis: Redis
     res: Response
 }
